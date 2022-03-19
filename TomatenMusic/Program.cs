f7f0c905700b8d0baf4d26b66ef8795754de84a1
Tim Müller <ekicilp@gmail.com>
2022-03-19 22:38:07 +0100

    +semver: patch

--- TomatenMusic/Program.cs
+++ TomatenMusic/Program.cs
@@ -4,7 +4,6 @@ using TomatenMusic_Api.Auth.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddCors();
